0385156c8377030f8188dba490df5ea044d24d49
MoroderMind <moroderwow@gmail.com>
2025-03-24 16:41:15 +0100

    init

--- Models/Product.cs
+++ Models/Product.cs
@@ -4,6 +4,9 @@
     {
         public int Id{ get; set; }
         public string Name{ get; set; }
+        public double Price { get; set; }
         public bool IsActive { get; set; }
+        public IEnumerable<ProductProp> ProductProperties { get; set; }
+
     }
 }
